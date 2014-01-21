710198bcfbd16963faa10894d21740733fec2144
Alfonso Etxeberria <alfonso@Arambi.local>
2014-01-21 11:56:02 +0400

    Mejorado el Recaptcha Component

--- Controller/Component/RecaptchaComponent.php
+++ Controller/Component/RecaptchaComponent.php
@@ -84,6 +84,7 @@ class RecaptchaComponent extends Component {
 		$this->_defaults['modelClass'] = $this->Controller->modelClass;
 		$this->settings = array_merge($this->_defaults, $settings);
 		$this->actions = array_merge($this->actions, $this->settings['actions']);
+		
 		unset($this->settings['actions']);
 	}
 
@@ -107,6 +108,11 @@ class RecaptchaComponent extends Component {
 		if (empty($this->privateKey)) {
 			throw new Exception(__d('recaptcha', "You must set your private recaptcha key using Configure::write('Recaptcha.privateKey', 'your-key');!", true));
 		}
+		
+		$this->Controller->{$this->Controller->modelClass}->validator()->add( 'recaptcha', array(
+		    'rule' => array( 'equalTo', 1),
+		    'message'=> __d( 'recaptcha', 'Por favor, escribe el código correctamente')
+		));
 	}
 
  /**
@@ -122,6 +128,16 @@ class RecaptchaComponent extends Component {
 		));
 
 		$this->Controller->{$modelClass}->recaptcha = true;
+		
+		if( $this->verify())
+    {
+      $this->Controller->request->data [$this->Controller->{$modelClass}->alias]['recaptcha'] = 1;
+    }
+    else
+    {
+      $this->Controller->request->data [$this->Controller->{$modelClass}->alias]['recaptcha'] = 0;
+    }
+		
 		if (in_array($this->Controller->action, $this->actions)) {
 			if (!$this->verify()) {
 				$this->Controller->{$modelClass}->recaptcha = false;
@@ -162,6 +178,8 @@ class RecaptchaComponent extends Component {
 
 			return false;
 		}
+		
+		return true;
 	}
 
 /**
